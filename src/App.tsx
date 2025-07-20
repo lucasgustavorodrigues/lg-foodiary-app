e563ea3252b3564b0f2174ae5375d6264f69b2e9
Lucas Gustavo <lg.rodrigues@outlook.com>
2025-07-20 10:08:55 -0300

    add custom font-family

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold
+} from "@expo-google-fonts/host-grotesk";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-import './styles/global.css'
+import { useFonts } from '@expo-google-fonts/host-grotesk';
+import { useEffect } from 'react';
+import './styles/global.css';
+
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View className='bg-lime-500 flex-1 items-center justify-center'>
       <Text className='text-base'>Open up App.tsx to start working on your app!</Text>
